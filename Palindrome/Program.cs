20585ac7fe364f8b321943a490400dda22f32378
Heorhii Ziziukin <Heorhii.Ziziukin@teaminternational.com>
2021-08-23 14:32:07 +0300

    First commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите слово: ");
+            string input = Console.ReadLine();
+
+            char[] arr = new char[input.Length];
+
+            Word word = new Word(input, arr);
+
+            if (word.IsPalindrome())
+            {
+                Console.WriteLine("Это слово является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Это слово НЕ является палиндромом");
+            }
+        }
+    }
+}
---
+++ Palindrome/Word.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome
+{
+    class Word
+    {
+        private string word;
+        private string reverseWord;
+        private char[] arr;
+
+        public Word(string word, char[] arr)
+        {
+            this.word = word;
+            this.arr = arr;
+        }
+
+        public bool IsPalindrome()
+        {
+            for (int i = 0; i < word.Length; ++i)
+            {
+                arr[i] = word[word.Length - i - 1];
+            }
+
+            reverseWord = new string(arr);
+
+            if (word == reverseWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
